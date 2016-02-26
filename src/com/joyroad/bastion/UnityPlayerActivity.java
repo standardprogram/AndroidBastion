b01b325440d5925e708f926f917e6343b8fef43e
carbon <standardprogram@foxmail.com>
2016-02-26 12:20:22 +0800

    first

---
+++ src/com/joyroad/bastion/UnityPlayerActivity.java
@@ -0,0 +1,8 @@
+package com.joyroad.bastion;
+
+import com.unity3d.player.*;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
